0595ff3ef70db028f44ba5811441783dfe175863
AR4T1C <35537651+GDCurve@users.noreply.github.com>
2024-10-30 18:23:08 +0200

    Fixes

--- static/script.js
+++ static/script.js
@@ -13,3 +13,26 @@ function newTab(url) {
     window.open(url);
 }
 
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+    const sendButton = document.getElementById("sending");
+
+    fetch("/send-email", {  
+        method: "POST",
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.message === "ziņa nosūtīta") { 
+            sendButton.textContent = "Ziņa nosūtīta";
+            sendButton.disabled = true;
+        }
+    })
+    .catch(error => {
+        console.error("err:", error);
+        alert("err");
+    });
+});
+
